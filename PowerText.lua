a1bddd24ac4a767547a8a0965c4913e46028ffed
Sodatha <oyunparki@proton.me>
2023-08-24 04:21:36 +0300

    first

---
+++ PowerText.lua
@@ -0,0 +1,94 @@
+PowerTextOptionsDefaults = {
+	Width = 128,
+	Height = 32,
+	X = 0,
+	Y = 0,
+}
+
+
+local PowerTextFrame = CreateFrame("Frame", "PowerTextFrame", UIParent)
+PowerTextFrame:SetWidth(120) 
+PowerTextFrame:SetHeight(42) 
+
+PowerTextFrame:SetMovable(true)
+PowerTextFrame:EnableMouse(true)
+PowerTextFrame:RegisterForDrag("LeftButton")
+PowerTextFrame:SetScript("OnDragStart", function() PowerTextFrame:StartMoving() end)
+PowerTextFrame:SetScript("OnDragStop", function() PowerTextFrame:StopMovingOrSizing() end)
+
+PowerTextFrame:SetBackdrop({
+	bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+	edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+	edgeSize = 8,
+	insets = { left = 4, right = 4, top = 4, bottom = 4 },
+})
+PowerTextFrame:SetBackdropColor(0, 0, 1, .3)
+
+PowerTextFrame:SetPoint("CENTER", 0, 0)
+PowerTextFrame:Show()
+
+PowerTextFrame:SetScript("OnLoad", PowerText_EventFrame_OnLoad)
+
+
+local function PowerText_EventFrame_OnEvent() 	
+	if event == "VARIABLES_LOADED" then
+		this:UnregisterEvent("VARIABLES_LOADED")		
+		PowerText_Initialize()	
+	elseif event == "UNIT_MANA" or event == "UNIT_ENERGY" or event == "UNIT_RAGE" then		
+		local base, posBuff, negBuff = UnitAttackPower("player");
+		local effective = base + posBuff + negBuff;
+		local mana = UnitMana("player").." / "..UnitManaMax("player")
+		PowerTextFrame.text:SetText(effective.." AP\n"..mana)		
+	elseif event == "UNIT_ATTACK_POWER" then		
+		local base, posBuff, negBuff = UnitAttackPower("player");
+		local effective = base + posBuff + negBuff;
+		local mana = UnitMana("player").." / "..UnitManaMax("player")
+		PowerTextFrame.text:SetText(effective.." AP\n"..mana)		
+	end
+	
+end
+
+PowerTextFrame:SetScript('OnEvent', function()
+    --PowerText_DisplayMessage(event)
+    PowerText_EventFrame_OnEvent()
+end)
+PowerTextFrame:RegisterEvent("VARIABLES_LOADED")
+PowerTextFrame:RegisterEvent("ADDON_LOADED")
+PowerTextFrame:RegisterEvent("UNIT_MANA")	
+PowerTextFrame:RegisterEvent("UNIT_ENERGY")	
+PowerTextFrame:RegisterEvent("UNIT_RAGE")	
+PowerTextFrame:RegisterEvent("UNIT_ATTACK_POWER")	
+
+function PowerText_EventFrame_OnLoad()	
+	
+end
+
+function PowerText_Initialize()
+	PowerTextFrame.text = PowerTextFrame:CreateFontString(nil,"ARTWORK") 
+	PowerTextFrame.text:SetFont("Fonts\\ARIALN.ttf", 230, "OUTLINE")
+	PowerTextFrame.text:SetPoint("CENTER",0,0)
+	PowerTextFrame.text:SetTextHeight(20)	
+	--PowerTextFrame.text:SetText(UnitMana("player").." / "..UnitManaMax("player"))
+	
+	local base, posBuff, negBuff = UnitAttackPower("player");
+	local effective = base + posBuff + negBuff;
+	local mana = UnitMana("player").." / "..UnitManaMax("player")
+	PowerTextFrame.text:SetText(effective.." AP\n"..mana)				
+end
+
+function PowerText_Toggle()
+	if PowerTextFrame:IsVisible() then
+		PowerTextFrame:Hide()
+	else
+		PowerTextFrame:Show()
+	end
+end
+
+function PowerText_DisplayMessage(message, r, g, b)
+	if r == nil then
+		DEFAULT_CHAT_FRAME:AddMessage(message, 0.53, 0.53, 0.93) 
+	else
+		DEFAULT_CHAT_FRAME:AddMessage(message, r, g, b) 
+	end		
+end
+
---
+++ README.md
@@ -0,0 +1,4 @@
+Wow 1.12 addon for melee
+Displays melee attack power and rage/energy
+
+Sodatha the mage in Turtle Wow
